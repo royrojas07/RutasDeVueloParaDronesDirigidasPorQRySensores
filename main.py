94c68075a31699f7a2b6e890ec919cc2a13bc8d3
ccromer <carlos.urenaalfaro@gmail.com>
2021-09-22 11:25:57 -0600

    primer version del main y Controlator

---
+++ main.py
@@ -0,0 +1,36 @@
+from djitellopy import Tello
+from threading import Thread
+from time import sleep
+import queue
+from Controlator import *
+
+#dron = Tello()
+#dron.connect()
+dron = "hola"
+cam_con_queue = queue.Queue() #cola entre la camara y el controlador
+sen_con_queue = queue.Queue() #cola entre el sensor y el controlador
+
+def main():
+    threads = [Thread( target=init_controlator, args=()),
+               Thread( target=init_camera, args=()),
+               Thread( target=init_sensor, args=())]
+
+    for thread in threads:
+        thread.start()
+    
+
+def init_controlator():
+    controlator = Controlator(dron,cam_con_queue,sen_con_queue)
+    controlator.start_processing()
+
+def init_camera():
+    print("hilo de camara")
+    sleep(5)
+    cam_con_queue.put("U:30,RR:20")
+
+
+def init_sensor():
+    print("hilo de sensor")
+
+if __name__ == '__main__':
+	main()
