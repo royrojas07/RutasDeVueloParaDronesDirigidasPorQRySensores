409c5a4c1a3aa2b80182989cae7844ed7a0fdb1a
fjimenez1999 <fjimenez1999@hotmail.com>
2021-09-23 18:37:10 -0600

    Inicio de la clase sensor_reader, faltan pruebas para establecer los umbrales

---
+++ Sensor_reader.py
@@ -0,0 +1,33 @@
+from grovepi import *
+import grovepi
+import time
+ULTRASONIC_RANGER = 8
+SOUND_SENSOR = A0
+ULTRASONIC_RANGER_LED = 4
+SOUND_SENSOR_LED = 3
+
+class Sensor_reader:
+    def __init__(self,sen_con_queue):
+        self.sen_con_queue = sen_con_queue
+    def start(distance_forward, distance_left, distance_right):
+        while True:
+            try:
+                distance = ultrasonicRead(ultrasonic_ranger)
+                loudness = analogRead(SOUND_SENSOR)
+                print(distance,' cm')
+                print("loudness = ", loudness)
+                if distance <= 20 and loudness >= 800:
+                    digitalWrite(ULTRASONIC_RANGER_LED,HIGH)
+                    digitalWrite(SOUND_SENSOR_LED,HIGH)
+                elif distance <= 20 and loudness < 800:
+                    digitalWrite(ULTRASONIC_RANGER_LED,HIGH)
+                    digitalWrite(SOUND_SENSOR_LED,LOW)
+                elif distance >20 and loudness >= 800:
+                    digitalWrite(ULTRASONIC_RANGER_LED,LOW)
+                    digitalWrite(SOUND_SENSOR_LED,HIGH)
+                else: 
+                    digitalWrite(ULTRASONIC_RANGER_LED,LOW)
+                    digitalWrite(SOUND_SENSOR_LED,LOW)
+                time.sleep(.5)
+            except IOError:
+                print("Error")
