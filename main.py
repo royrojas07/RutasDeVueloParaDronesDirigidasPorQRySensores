b39a2c3e2e8c6343f3dcc2fc74a842bf1dbbb631
ccromer <carlos.urenaalfaro@gmail.com>
2021-10-09 21:25:27 -0600

    signal handler

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from djitellopy import Tello
-from threading import Thread
+import threading
 from time import sleep
 import queue
 import sys
+import signal
 from Controller import *
 from ImageCaption import *
 
 dron = Tello()
 dron.connect()
+exit_event = threading.Event()
 cam_con_queue = queue.Queue() #cola entre la camara y el controlador
 sen_con_queue = queue.Queue() #cola entre el sensor y el controlador
 max_height = int(sys.argv[1])
 #print( dron.get_battery() )
+
+def handler(signum, frame):
+    print("[ABORT] Landing the Tello")
+    dron.land()
+    exit_event.set() # para que mate los threads
+    exit()
+
+
 def main():
     threads = [Thread( target=init_Controller, args=()),
                Thread( target=init_camera, args=()),
@@ -20,6 +30,7 @@ def main():
     for thread in threads:
         thread.start()
     
+    signal.signal(2, handler)
 
 def init_Controller():
     controller = Controller(dron,cam_con_queue,sen_con_queue)
